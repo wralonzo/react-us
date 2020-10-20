e47d55727f911bbcc9d3df82fe1f30f567f71eb6
Willian Alonzo <williamalonzo4519@gmail.com>
2020-10-20 12:55:06 -0600

    Updates

---
+++ precache-manifest.b50d94739c668bb9e44fcc4f17e603a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "920e461bfc33eb8648fcad810f198737",
+    "url": "/react-us/index.html"
+  },
+  {
+    "revision": "6ef5f4844e78c45b2880",
+    "url": "/react-us/static/css/main.5e0111df.chunk.css"
+  },
+  {
+    "revision": "d29502ca971cd6b1ec0d",
+    "url": "/react-us/static/js/2.d846c26a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-us/static/js/2.d846c26a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ef5f4844e78c45b2880",
+    "url": "/react-us/static/js/main.f9911c9a.chunk.js"
+  },
+  {
+    "revision": "95adbe30462334ec2535",
+    "url": "/react-us/static/js/runtime-main.b81fa43d.js"
+  }
+]);
